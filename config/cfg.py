282b4efe878a1446251095ab56421138bd36db56
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-01-08 15:17:23 +0800

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -10,7 +10,7 @@ cfg = {
     # 'template': '[X1] [X2]? [MASK].',
     'template': '[X1] ? [MASK] , [X2]',
     'answer': ['No', 'Yes'],
-    'device': 'cuda',
+    'device': 'TPU',
     'optimizer': 'Adam',
     'word_size': 50265
 }
