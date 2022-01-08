47d059dfa4cc22469a40f3974f62cc855cd94f5e
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-01-08 19:58:55 +0800

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -1,10 +1,10 @@
 cfg = {
     'gpu_id': 0,
     'max_len': 128,
-    'train_batch_size': 32,
+    'train_batch_size': 16,
     'test_batch_size': 16 * 5,
     'learning_rate': 1e-5,
-    'epoch': 10,
+    'epoch': 5,
     'K': 16,
     # 'Kt': 2000,
     # 'template': '[X1] [X2]? [MASK].',
