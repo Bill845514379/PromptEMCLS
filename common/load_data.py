5723239aacf266c2028342ab5b7ac3ed75ff0b03
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-01-12 13:05:41 +0800

    Update load_data.py

--- common/load_data.py
+++ common/load_data.py
@@ -2,13 +2,12 @@
 
 import numpy as np
 from config.cfg import path, cfg
-from common.util import delete_character, delete_word, reorder_span, reorder_words
 from pytorch_transformers import RobertaTokenizer
+
 tokenizer = RobertaTokenizer.from_pretrained(path['roberta_path'])
 
 
 def load_data(path):
-
     data_X, y = [], []
     with open(path, 'r', encoding='utf-8') as file:
         for line in file:
@@ -18,7 +17,7 @@ def load_data(path):
 
             for i in range(1, 6):
                 if str(i) in line[0]:
-                    y.append(i-1)
+                    y.append(i - 1)
     return data_X, y
 
 
@@ -29,6 +28,7 @@ def get_random_sample_ids(length, K):
         ids_list.append(i)
     ids = random.sample(ids_list, K)
     return ids
+
 
 def generate_template(data_X_1, data_X_2, data_y_1, data_y_2, is_train=False):
     '''
@@ -52,7 +52,7 @@ def generate_template(data_X_1, data_X_2, data_y_1, data_y_2, is_train=False):
             template = template.replace('[X2]', SEP + ' ' + data_X_2[j] + ' ' + SEP)
             template = template.replace('[MASK]', MASK)
             data_X.append(template)
-            
+
             if data_y_1[i] == data_y_2[j]:
                 data_y.append(1)
                 if is_train:
@@ -74,6 +74,7 @@ def generate_template(data_X_1, data_X_2, data_y_1, data_y_2, is_train=False):
 
     return data_X, data_y
 
+
 def data_split(data_X, data_y, K=8, Kt=8):
     train_ids = get_random_sample_ids(len(data_X), K)
     train_X, train_y = [], []
@@ -94,6 +95,7 @@ def data_split(data_X, data_y, K=8, Kt=8):
             test_y.append(test_all_y[i])
 
     return np.array(train_X), np.array(train_y), np.array(test_X), np.array(test_y)
+
 
 def data_split_all(data_X, data_y, label_size, K=8, Kt=8):
     data_set_X, data_set_y = {}, {}
@@ -119,6 +121,47 @@ def data_split_all(data_X, data_y, label_size, K=8, Kt=8):
             test_y = np.hstack([test_y, test_y_t])
 
     return train_X, train_y, test_X, test_y
+
+
+def data_split_balance(data_X, data_y, pos_id, num):
+    '''
+    正例比负例少， 随机选
+    :param data_X:
+    :param data_y:
+    :return:
+    '''
+
+    pos_set_X, pos_set_y = [], []
+    neg_set_X, neg_set_y = [], []
+    pos_type = -1
+    for i in range(len(data_X)):
+        if data_y[i] == pos_id:
+            pos_type = i // cfg['K']
+            break
+
+    for i in range(len(data_X)):
+        if data_y[i] == pos_id:
+            pos_set_X.append(data_X[i])
+            pos_set_y.append(data_y[i])
+        else:
+            if abs(i//cfg['K'] - pos_type) > 0:
+                neg_set_X.append(data_X[i])
+                neg_set_y.append(data_y[i])
+
+    pos_set_ids = get_random_sample_ids(len(pos_set_X), num)
+    neg_set_ids = get_random_sample_ids(len(neg_set_X), num)
+
+    data_X, data_y = [], []
+
+    for i in pos_set_ids:
+        data_X.append(pos_set_X[i])
+        data_y.append(pos_set_y[i])
+
+    for i in neg_set_ids:
+        data_X.append(neg_set_X[i])
+        data_y.append(neg_set_y[i])
+
+    return data_X, data_y
 
 
 
